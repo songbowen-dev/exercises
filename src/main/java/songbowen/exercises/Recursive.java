84645d3409f6cb2d500239be74b44ed6e0b86cc6
songbowen <songbowen010@icloud.com>
2022-06-07 01:14:54 +0800

    add

--- src/main/java/songbowen/exercises/Recursive.java
+++ src/main/java/songbowen/exercises/Recursive.java
@@ -5,22 +5,6 @@ import java.util.stream.Collectors;
 
 public class Recursive {
 
-    /**
-     * 一个整数数组，长度为n，将其分为m 份，使各份的和相等，求m 的最大值
-     * 比如数组：{3,2,4,3,6}
-     * m=1，可以分成{3，2，4，3，6} ;
-     * m=2，可以分成{3,6}{2,4,3} ；
-     * m=3，可以分成{3,3}{2,4}{6}
-     * 所以m 的最大值为3
-     */
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        int[] in = Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).toArray();
-        maxShare(in);
-    }
-
-
     static boolean isShare(int[] in, int[] group, int num, int groupSize, int curSize, int groupId) {
         if (curSize == 0) {
             if (groupId == num) {
@@ -47,6 +31,14 @@ public class Recursive {
         return false;
     }
 
+    /**
+     * 一个整数数组，长度为n，将其分为m 份，使各份的和相等，求m 的最大值
+     * 比如数组：{3,2,4,3,6}
+     * m=1，可以分成{3，2，4，3，6} ;
+     * m=2，可以分成{3,6}{2,4,3} ；
+     * m=3，可以分成{3,3}{2,4}{6}
+     * 所以m 的最大值为3
+     */
     static void maxShare(int[] in) {
         int sum = Arrays.stream(in).sum();
         int maxNum = sum / Arrays.stream(in).max().getAsInt();
@@ -74,5 +66,34 @@ public class Recursive {
             }
         }
         System.out.println("{" + Arrays.stream(in).mapToObj(Objects::toString).collect(Collectors.joining(",")) + "}");
+    }
+
+    /**
+     * 求集合所有的子集
+     * 例如 [1,2] 的子集为 [] [1] [2] [1,2]
+     */
+    static List<Set<Integer>> subSet(Set<Integer> set) {
+        List<Set<Integer>> result = new ArrayList<>();
+        result.add(new TreeSet<>());
+        for (Integer value : set) {
+            List<Set<Integer>> tmp = new ArrayList<>();
+            for (Set<Integer> subSet : result) {
+                TreeSet<Integer> newSet = new TreeSet<>(subSet);
+                newSet.add(value);
+                tmp.add(newSet);
+                newSet = new TreeSet<>(subSet);
+                tmp.add(newSet);
+            }
+            result = tmp;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeSet<Integer> set = new TreeSet<>();
+        set.add(1);
+        set.add(2);
+        List<Set<Integer>> sets = subSet(set);
+        System.out.println(sets);
     }
 }
